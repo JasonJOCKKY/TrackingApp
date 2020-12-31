56be47d0c64b179c971a92f97900dcdb4421aef3
Tan Jingsong <tanjingsong@Tans-MacBook-Pro.local>
2020-12-30 22:58:30 -0600

    Map API Key instruction update

--- tracking_app/lib/main.dart
+++ tracking_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tracking_app/pages/doctor_map_page.dart';
+import 'package:tracking_app/pages/patient_map_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.brown,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: DoctorMapPage(),
+      home: PatientMapPage(),
     );
   }
 }
--- tracking_app/lib/pages/patient_map_page.dart
+++ tracking_app/lib/pages/patient_map_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tracking_app/pages/patient_menu_page.dart';
 import 'package:tracking_app/widgets/mapfab.dart';
+import 'package:tracking_app/widgets/topbar.dart';
 
 class PatientMapPage extends StatefulWidget {
   @override
@@ -9,14 +10,6 @@ class PatientMapPage extends StatefulWidget {
 
 class _PatientMapPageState extends State<PatientMapPage> {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
-
-  // String locationEnabled = "The location service is not enabled.";
-  // String locationString = "N/A";
-
-  // onLocationUpdate(LocationData newLocation) {
-  //   locationString =
-  //       "Latitude: ${newLocation.latitude}\nLongitude: ${newLocation.longitude}";
-  // }
 
   // Functions for FABs
   bool isMapCentered = false;
@@ -43,31 +36,9 @@ class _PatientMapPageState extends State<PatientMapPage> {
 
   // Override Methods
   @override
-  void initState() {
-    super.initState();
-
-    // serviceCheckLocationEnabled().then((bool locationIsEnabled) {
-    //   if (locationIsEnabled) {
-    //     setState(() {
-    //       locationEnabled = "The location service is enabled!";
-    //     });
-
-    //     serviceLocationStream().listen((LocationData newLocation) {
-    //       setState(() {
-    //         onLocationUpdate(newLocation);
-    //       });
-    //     });
-    //   }
-    // });
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       key: this._scaffoldKey,
-      // appBar: AppBar(
-      //   toolbarHeight: 0,
-      // ),
       drawer: PatientMenuPage(),
       drawerEnableOpenDragGesture: false,
       body: Stack(
@@ -84,54 +55,61 @@ class _PatientMapPageState extends State<PatientMapPage> {
             ),
           ),
           // FABs
-          SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.all(30),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      // Menu Button
-                      MapFab(
-                        onPressed: () {
-                          this._scaffoldKey.currentState.openDrawer();
-                        },
-                        icon: Icons.menu,
-                        isActive: false,
-                      ),
-                      // Center Button
-                      MapFab(
-                        onPressed: this.onCenterToggle,
-                        icon: Icons.near_me,
-                        isActive: this.isMapCentered,
-                      ),
-                    ],
-                  ),
-                  Column(
-                    verticalDirection: VerticalDirection.up,
-                    children: [
-                      MapFab(
-                          onPressed: this.onGridToggle,
-                          icon: Icons.visibility,
-                          isActive: this.isGridVisible),
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: 10),
-                        child: MapFab(
-                          onPressed: this.onTimelineTogger,
-                          icon: Icons.timeline,
-                          isActive: this.isTimelineShown,
-                          mini: true,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
+          _fabs(),
+          // Top Bar
+          TopBar(),
         ],
+      ),
+    );
+  }
+
+  // Build Functions
+  Widget _fabs() {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.all(30),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                // Menu Button
+                MapFab(
+                  onPressed: () {
+                    this._scaffoldKey.currentState.openDrawer();
+                  },
+                  icon: Icons.menu,
+                  isActive: false,
+                ),
+                // Center Button
+                MapFab(
+                  onPressed: this.onCenterToggle,
+                  icon: Icons.near_me,
+                  isActive: this.isMapCentered,
+                ),
+              ],
+            ),
+            Column(
+              verticalDirection: VerticalDirection.up,
+              children: [
+                MapFab(
+                    onPressed: this.onGridToggle,
+                    icon: Icons.visibility,
+                    isActive: this.isGridVisible),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 10),
+                  child: MapFab(
+                    onPressed: this.onTimelineTogger,
+                    icon: Icons.timeline,
+                    isActive: this.isTimelineShown,
+                    mini: true,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
