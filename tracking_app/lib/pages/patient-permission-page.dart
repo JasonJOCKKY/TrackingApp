4308076d5a455df7e83c83c1bc4f6d75471f49c4
Jingsong Tan <jasontanjingsong@gmail.com>
2021-01-06 20:16:19 -0600

    Permission Page Layout

--- tracking_app/lib/pages/patient-permission-page.dart
+++ tracking_app/lib/pages/patient-permission-page.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -14,47 +16,103 @@ class _PatientPermissionPageState extends State<PatientPermissionPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        toolbarHeight: 0,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
+      body: SingleChildScrollView(
         child: Column(
           children: [
-            Text("Location Permission Denied"),
-            Text(
-                "Dear user, our app constantly collects your location data in the background and send it to our server in order to provide more accurate desease prevention data.  In order to use this app, please enable your cellphone's location service:\n1. Enable your cellphone's location service.\n2. Grant this app the permission to always access your location."),
-            SettingButton(
-              onPressed: null,
-              lable: "Enable Location Service",
-            ),
-            SettingButton(
-              onPressed: null,
-              lable: "Grant Constant Permission",
+            HeaderBar(),
+            Padding(
+              padding: EdgeInsets.fromLTRB(
+                20,
+                20,
+                20,
+                MediaQuery.of(context).padding.bottom,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Dear User,",
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    "Our app constantly collects your location data in the background and send it to our server in order to provide more accurate disease prevention data.  In order to use this app, please enable the following on your cellphone:",
+                    style: Theme.of(context).textTheme.subtitle1,
+                  ),
+                  SizedBox(height: 20),
+                  ExpansionPanelList(
+                    expansionCallback: (panelIndex, isExpanded) {
+                      log("expansion");
+                    },
+                    children: [
+                      expPanel(
+                        onTap: null,
+                        label: "Enable Location Service",
+                        icon: Icon(Icons.check_box_outline_blank),
+                        body: "Enable your cellphone's location service.",
+                      ),
+                      expPanel(
+                        onTap: null,
+                        label: "Grant Constant Permission",
+                        icon: Icon(Icons.check_box_outline_blank),
+                        body:
+                            "Grant this app the permission to always access your location.",
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 50),
+                  ElevatedButton.icon(
+                    onPressed: () {},
+                    icon: Icon(Icons.logout),
+                    label: Text("Log Out"),
+                  ),
+                  SizedBox(height: 50),
+                ],
+              ),
             ),
           ],
         ),
       ),
     );
   }
+
+  // Custom build methods
+  ExpansionPanel expPanel(
+      {Function onTap, Icon icon, String label, String body}) {
+    return ExpansionPanel(
+      isExpanded: true,
+      headerBuilder: (context, isExpanded) {
+        return ListTile(
+          onTap: () {},
+          title: Text(label),
+          leading: Icon(
+            Icons.check_box_outline_blank,
+          ),
+        );
+      },
+      body: ListTile(
+        title: Text(
+            "Grant this app the permission to always access your location."),
+      ),
+    );
+  }
 }
 
-class SettingButton extends StatelessWidget {
-  final Function() onPressed;
-  final String lable;
-
-  const SettingButton({Key key, this.onPressed, this.lable}) : super(key: key);
-
+class HeaderBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final double _width = MediaQuery.of(context).size.width - 50;
+    double topPadding = MediaQuery.of(context).padding.top;
 
-    return ElevatedButton.icon(
-      onPressed: () {},
-      icon: Icon(Icons.check_box_outline_blank),
-      label: Text(lable),
-      // color: Theme.of(context).primaryColor,
-      // textColor: Theme.of(context).canvasColor,
+    return Container(
+      padding: EdgeInsets.fromLTRB(10, 10 + topPadding, 10, 10),
+      child: Text(
+        "Location Permission Denied",
+        style: Theme.of(context)
+            .primaryTextTheme
+            .headline2
+            .apply(color: Colors.white),
+      ),
+      decoration: BoxDecoration(color: Theme.of(context).primaryColor),
     );
   }
 }
